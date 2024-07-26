67014f74a16873520b8d2d9bf0fdd6576c15070f
Temisola Mayomikun Olajide <tmo2000@hw.ac.uk>
2024-07-26 10:45:12 +0100

    made minor changes to homeUpdate.jsx

--- src/pages/homeUpdate.jsx
+++ src/pages/homeUpdate.jsx
@@ -5,8 +5,8 @@ const HomeUpdate = () => {
   return (
     <div>
          <Navbar />
-      <div className="bg-[#c4cee9] flex flex-col items-center justify-center min-h-screen">
-        <div className="bg-[#5d6aa8] p-20 px-[4rem] rounded-md shadow-md text-center w-[70%] mb-8">
+      <div className="bg-[#c4cee9] flex flex-col items-center justify-center min-h-1/2">
+        <div className="bg-[#5d6aa8] p-20 pt-36 px-[4rem] rounded-md shadow-md text-center w-[70%] mb-0">
           <h1 className="text-white text-3xl mb-12">Find Your Dream Job</h1>
           <form className="flex justify-between gap-4">
             <select className="p-2 border w-full border-gray-300 rounded-md">
@@ -28,7 +28,7 @@ const HomeUpdate = () => {
           </form>
         </div>
         <div className=" p-8  text-center w-[50%]">
-          <h2 className="text-[#444] text-3xl mb-6">Explore Projects Opportunities</h2>
+          <h2 className="text-[#444] text-3xl mb-4">Explore Projects Opportunities</h2>
           <form className="flex justify-between gap-4">
             <select className="p-2 w-full border border-gray-300 rounded-md">
               <option>Option 1</option>
@@ -101,7 +101,7 @@ const HomeUpdate = () => {
       </div>
 
     <div className="flex items-center gap-4 p-4" style={{backgroundColor: "#c4cee9"}}>
-            <h2 className="text-black font-bold text-3xl mb-12 m-4 w-1/2">Find exceptional tech <br />talents from <br />anywhere in <br />the world</h2>
+            <div className="text-black font-bold text-3xl mb-12 m-4 w-1/2">Find exceptional tech <br/>talents from anywhere <br />in the world</div>
             <input
                 type="text"
                 placeholder="Search"
